88da5297e403118ba00e3485a936435fb2c3ee66
Allan <allandrs@gmail.com>
2024-02-12 18:29:20 -0300

    wip swagger config

--- src/main/java/com/allanDi/blogApi/config/SwaggerConfig.java
+++ src/main/java/com/allanDi/blogApi/config/SwaggerConfig.java
@@ -1,14 +1,16 @@
 package com.allanDi.blogApi.config;
 
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
 public class SwaggerConfig {
 
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.allanDi.blogApi.controller"))
                 .paths(PathSelectors.any())
