5498b045d07960ad7145c49debb936ef93f8d771
Allan <allandrs@gmail.com>
2024-02-12 12:36:14 -0300

    Initial commit

---
+++ src/main/java/com/allanDi/blogapi/model/Post.java
@@ -0,0 +1,23 @@
+package com.allanDi.blogapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime creatAt;
+    private LocalDateTime udatedAt;
+
+
+
+}
