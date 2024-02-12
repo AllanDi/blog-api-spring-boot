ae36d8f9552f892336c95b19f002f008dc1b23b0
Allan <allandrs@gmail.com>
2024-02-12 19:27:54 -0300

    wip swagger config

--- src/main/java/com/allanDi/blogApi/config/SwaggerConfig.java
+++ src/main/java/com/allanDi/blogApi/config/SwaggerConfig.java
@@ -1,19 +1,18 @@
 package com.allanDi.blogApi.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 public class SwaggerConfig {
 
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.allanDi.blogApi.controller"))
-                .paths(PathSelectors.any())
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("API")
+                .pathsToMatch("/api/**")
                 .build();
     }
 }
