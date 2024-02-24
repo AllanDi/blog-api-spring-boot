9f595377f39e00c6ec1698b600b741c345ed3e26
Allan <allandrs@gmail.com>
2024-02-24 09:00:04 -0300

    starting with unit tests

---
+++ src/test/java/com/allanDi/blogApi/service/PublicationServiceTest.java
@@ -0,0 +1,26 @@
+package com.allanDi.blogApi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+class PublicationServiceTest {
+
+    @Test
+    void createPublication() {
+    }
+
+    @Test
+    void readAllPublication() {
+    }
+
+    @Test
+    void updatePublication() {
+    }
+
+    @Test
+    void deletePublication() {
+    }
+}
