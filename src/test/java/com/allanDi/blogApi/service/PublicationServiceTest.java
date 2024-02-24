fbd969b442e21687076f8ad6279a1acd09e64623
Allan <allandrs@gmail.com>
2024-02-24 10:47:55 -0300

    wip unit tests

--- src/test/java/com/allanDi/blogApi/service/PublicationServiceTest.java
+++ src/test/java/com/allanDi/blogApi/service/PublicationServiceTest.java
@@ -1,12 +1,56 @@
 package com.allanDi.blogApi.service;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.allanDi.blogApi.model.Publication;
+import com.allanDi.blogApi.repository.Repository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class PublicationServiceTest {
+
+    @Mock
+    Repository repository;
+
+    @InjectMocks
+    private PublicationService service;
+    private Publication validPublication;
+
+    @BeforeEach
+    void setup(){
+        validPublication = new Publication();
+        validPublication.setTitle("Test title");
+        validPublication.setContent("Valid content");
+    }
+
+    @Test
+    void whenCreatePublicationWithValidPublication_thenSaveIsCalled(){
+
+        service.createPublication(validPublication);
+        verify(repository, times(1)).save(validPublication);
+    }
+
+    @Test
+    void whenCreatePublicationWithInvalidPublication_thenThrowException(){
+
+        Publication invalidPublication = new Publication();
+        assertThrows(IllegalArgumentException.class, () ->
+                service.createPublication(invalidPublication));
+
+    }
+
+
+
 
     @Test
     void createPublication() {
